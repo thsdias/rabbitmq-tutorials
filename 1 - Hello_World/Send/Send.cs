9f202d317c28f4e7ffdae12b6f097b9bbe9b5095
thsdias <thdias.84@gmail.com>
2023-03-05 20:37:53 -0300

    Hello World RabbitMQ

---
+++ 1 - Hello_World/Send/Send.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+const string Queue = "hello";
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: Queue,
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+const string message = "Hello World!";
+var body = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(exchange: string.Empty,
+                     routingKey: Queue,
+                     basicProperties: null,
+                     body: body);
+Console.WriteLine($" [x] Sent {message}");
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# rabbitmq-tutorials
+# RabbitMQ Tutorials
+
 These tutorials cover the basics of creating messaging applications using RabbitMQ.
+
+
+1 "Hello World!"
+The simplest thing that does something
